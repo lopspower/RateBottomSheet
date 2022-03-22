eb771c03cbb8f1f97847b852be29b39c66e1a81b
ilmirka <shgbievi@gmail.com>
2022-03-22 21:44:19 +0300

    added possibility to customize packageName or url; added AskInfo.kt and RateInfo.kt for customizing texts; refactoring.

---
+++ ratebottomsheet/src/main/java/com/mikhaellopez/ratebottomsheet/AskInfo.kt
@@ -0,0 +1,9 @@
+package com.mikhaellopez.ratebottomsheet
+
+data class AskInfo(
+    val title: String? = null,
+    val message: String? = null,
+    val laterText: String? = null,
+    val cancelText: String? = null,
+    val okText: String? = null
+)
